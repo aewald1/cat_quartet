b72ffd1c2fb4f99d739abe418076de5c580e4595
Arne <mail@aewald.net>
2020-01-19 19:41:11 +0100

    added 1st analysis script

---
+++ code/py3/main1.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 18 20:15:22 2020
+
+@author: Arne
+"""
+import os
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+file_in = r"C:\OneDrive\misc\katzenquartett\katzenquartett.csv"
+df = pd.read_csv(file_in, sep=',')
+
+sns.violinplot(data=df)
+
+#%%
+corr = df.corr()
+
+# Generate a mask for the upper triangle
+mask = np.zeros_like(corr, dtype=np.bool)
+mask[np.triu_indices_from(mask)] = True
+
+# Set up the matplotlib figure
+f, ax = plt.subplots(figsize=(11, 9))
+
+# Generate a custom diverging colormap
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+# Draw the heatmap with the mask and correct aspect ratio
+sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+
+
+#%%
+
+sns.lmplot(x="x", y="y", col="dataset", hue="dataset", data=df,
+           col_wrap=2, ci=None, palette="muted", height=4,
+           scatter_kws={"s": 50, "alpha": 1})
+
+#%%
+
+# Plot sepal with as a function of sepal_length across days
+g = sns.lmplot(x="Größe", y="Intelligenz",
+               truncate=True, height=5, data=df)
+
+# Use more informative axis labels than are provided by default
+#g.set_axis_labels("Sepal length (mm)", "Sepal width (mm)")
+
